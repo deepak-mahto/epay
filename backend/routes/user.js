bc7f5823eadc8490144acd06c8b4dff65f8c2ccb
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-15 13:38:12 +0530

    Backend auth route signin section

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,11 +11,11 @@ const signupBody = zod.object({
   password: zod.string,
 });
 
-userRouter.get("/signup", async (req, res) => {
+userRouter.post("/signup", async (req, res) => {
   const { success } = signupBody.safeParse(req.body);
 
   if (!success) {
-    res.status(411).json({
+    return res.status(411).json({
       message: "Email already taken or inputs are incorrect",
     });
   }
@@ -24,7 +24,7 @@ userRouter.get("/signup", async (req, res) => {
   });
 
   if (existingUser) {
-    res.status(411).json({
+    return res.status(411).json({
       message: "Email already taken or inputs are incorrect",
     });
   }
@@ -48,6 +48,44 @@ userRouter.get("/signup", async (req, res) => {
   res.json({
     message: "User created successfully",
     token: token,
+  });
+});
+
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+userRouter.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token: token,
+    });
+    return;
+  }
+
+  res.status(411).json({
+    message: "Error while logging in",
   });
 });
 
