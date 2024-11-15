0ec3411fd5e4cc1d62b6ccf85be030256507ebcb
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-15 10:10:56 +0530

    Backend auth route - signup endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,53 @@
 const { Router } = require("express");
+const { User } = require("../db");
+const JWT_SECRET = require("../config");
+const zod = require("zod");
 const userRouter = Router();
 
-userRouter.get("/signup", (req, res) => {
+const signupBody = zod.object({
+  username: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string,
+  password: zod.string,
+});
+
+userRouter.get("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+
+  if (!success) {
+    res.status(411).json({
+      message: "Email already taken or inputs are incorrect",
+    });
+  }
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    res.status(411).json({
+      message: "Email already taken or inputs are incorrect",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+
+  const userId = user._id;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
   res.json({
-    msg: "You are signed up",
+    message: "User created successfully",
+    token: token,
   });
 });
 
