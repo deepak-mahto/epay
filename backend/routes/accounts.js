10deb7181670645079c57dc9a0e398aaa7a743f0
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-17 01:10:54 +0530

    Add an endpoint for user to get their balance

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,5 +1,17 @@
 const { Router } = require("express");
 const accountRouter = Router();
+const { Account } = require("../db");
+const { authMiddleware } = require("../middleware");
+
+accountRouter.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+
+  res.json({
+    balance: account.balance,
+  });
+});
 
 module.exports = {
   accountRouter: accountRouter,
