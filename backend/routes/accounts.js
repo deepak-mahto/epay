65705021969c2a257ade28f3fcfb227f57ba23a2
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-16 11:43:24 +0530

    Create a new router for accounts

---
+++ backend/routes/accounts.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const accountRouter = Router();
+
+module.exports = {
+  accountRouter: accountRouter,
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const mainRouter = Router();
 const { userRouter } = require("./user");
+const { accountRouter } = require("./accounts");
 
 mainRouter.use("/user", userRouter);
+mainRouter.use("/account", accountRouter);
 
 module.exports = {
   mainRouter,
