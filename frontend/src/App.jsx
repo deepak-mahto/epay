72744263f8f8b9705e37b1cdf9b7877c5fa8ccb7
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-17 23:27:09 +0530

    Add routing to react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Send from "./pages/Send";
+
 function App() {
   return (
     <>
-      <h1>Basic version of paytm</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Send />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
